fc70aea6755cd810b30d38a68867ea25b4507fb9
pminh <pminhkhue1308@gmail.com>
2025-05-15 17:06:28 +0200

    data structure

---
+++ js/data/projectsdata.js
@@ -0,0 +1,12 @@
+export const projectsdata = [
+    {id:"", name:"", title:"", description:"", description_paragraphs: [], images:[], url:"", languages:[], projecttype:""}
+];
+
+
+// list des projets à mettre :
+// - hackathon en web
+// - nuit du code en pygame
+// - assosapp
+// - sudoku game en visual basic
+// - site web de restaurant (à migrer vers hebergeur gratuit)
+// - site de QCM (à mettre import export)
